33d0966235270c025081328f81c6a67332a9908d
michael GALSERAN <michael.galseran@epitech.eu>
2020-04-08 11:59:05 +0200

    Room creation route added

--- BizkitREST/index.js
+++ BizkitREST/index.js
@@ -2,12 +2,22 @@
 var express = require("express");
 var app = express();
 
+
+/* List of current connected players */
 let user = []
+
+/* List of available game room */
+let rooms = []
+
+
 let current_user_id = 1;
+let current_room_id = 1;
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
+/* POST REQUEST */
 
 app.post("/user", (req, res) => {
     if (!req.query.name)
@@ -17,18 +27,49 @@ app.post("/user", (req, res) => {
             message: "Missing name"
         }))
     }
-    user.push({
+    new_user = {
         name: req.query.name,
         id: current_user_id,
-    })
+    }
+    user.push(new_user);
     current_user_id += 1;
-    console.log(user);
     return res.send(JSON.stringify({
         code: 200,            
-        user: {
-            name: req.query.name,
-            id: current_user_id,
-        },
+        user:new_user,
         message: "User successfully created" 
     }))
 })
+
+app.post("/room", (req, res) => {
+    if (!req.query.name || !req.query.user_id)
+    {
+        return res.send(JSON.stringify({
+            code: 400,
+            message: "Bad request",
+            query: req.query,
+        }))
+    }
+    new_room = {
+        name: req.query.name,
+        id: current_room_id,
+        user_list: [
+
+        ],
+        turn_direction: "clockwise",
+        dice_values: []
+    }
+    new_room.user_list.push(user.find(usr => usr.id == req.query.user_id).name);
+    rooms.push(new_room);
+   
+    current_room_id += 1;
+    return (res.send(JSON.stringify({
+        code: 200,
+        message: "Room successfully created",
+        data: new_room
+    })))
+})
+
+/* GET REQUEST */
+
+
+/* PUT REQUEST */
