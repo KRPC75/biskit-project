79676b0250a1a836fd54ebd82d9a4df5f7a186f2
KRPC75 <63301432+KRPC75@users.noreply.github.com>
2020-04-08 19:48:24 +0200

    Commentaire de la classe Turn

--- Biskit/src/main/com/dices/biskit/core/Turn.java
+++ Biskit/src/main/com/dices/biskit/core/Turn.java
@@ -7,30 +7,55 @@ import com.dices.biskit.models.Result;
 import com.dices.biskit.models.dices.Dice;
 import com.dices.biskit.models.rules.DiceRule;
 import com.dices.biskit.models.rules.DoubleRule;
-import com.dices.biskit.models.rules.PlayerRule;
 import com.dices.biskit.models.rules.Rule;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
-@Controller
+/**
+ *
+ * Classe gérant le tour d'un joueur
+ *
+ */
+@Component
 public class Turn {
 
+    /**
+     * Exécution du tour
+     *
+     * @param player        Joueur devant jouer
+     * @param gameDices     Liste des dés de la partie
+     * @param gameRules     Liste des règles de la partie
+     * @param playerList    Liste des joueurs de la partie
+     */
     public void doTurn(Player player, List<Dice> gameDices, List<Rule> gameRules, List<Player> playerList) {
         Result result = throwDices(gameDices);
         List<Rule> rulesToApply = defineRulesToApply(result, player, gameRules);
         applyRules(rulesToApply, gameRules, player, playerList);
     }
 
+    /**
+     * Lancement des dés
+     *
+     * @param diceList  Liste des dés à jeter
+     * @return          Le Résultat du lancer de dé
+     */
     Result throwDices(List<Dice> diceList) {
         Result result = new Result(diceList);
         result.throwDices();
         return result;
     }
 
+    /**
+     * Définition des règles à appliquer sur le résultat du lancé de dé
+     *
+     * @param result    Résultat du lancé de dé
+     * @param player    Joueur ayant jeté les dés
+     * @param ruleList  Liste des règles du jeu
+     * @return          Liste des règles à appliquer pour le lancé de dé
+     */
     List<Rule> defineRulesToApply(Result result, Player player, List<Rule> ruleList) {
 
         List<Rule> rulesToApply = new ArrayList<>();
@@ -58,6 +83,14 @@ public class Turn {
         return rulesToApply;
     }
 
+    /**
+     * Application des règles associées au lancé
+     *
+     * @param rulesToApply  Liste des règles à appliquer
+     * @param rules         Liste des règles
+     * @param player        Joueur ayant lancé les dés
+     * @param playerList    Liste des joueurs
+     */
     void applyRules(List<Rule> rulesToApply, List<Rule> rules, Player player, List<Player> playerList) {
         List<SpecialEffect> specialEffects = new ArrayList<>();
         for (Rule rule : rulesToApply) {
@@ -71,6 +104,8 @@ public class Turn {
                     break;
                 case NEW:
                     break;
+                case PASS:
+                    break;
                 default:
                     break;
             }
@@ -78,9 +113,18 @@ public class Turn {
         }
     }
 
+    /**
+     * Gestion des prisons
+     *
+     * @param rule          Règle de prison à créer
+     * @param player        Joueur entrant en prison
+     * @param ruleList      Liste des règles de la partie
+     * @param playerList    Liste des joueurs de la partie
+     */
     void manageJail(Rule rule, Player player, List<Rule> ruleList, List<Player> playerList) {
         DoubleRule doubleRule = (DoubleRule) rule;
 
+        // Récupération du potentiel joueur en prison
         Player jailedPlayer = playerList.stream()
                 .filter(jPlayer -> jPlayer.hasJail(doubleRule.getDoubleValue()))
                 .findFirst().orElse(null);
@@ -88,6 +132,7 @@ public class Turn {
 
         if (null != jailedPlayer) {
             jailedPlayer.removeJail(doubleRule.getDoubleValue());
+            // Suppression de la règle de dé représentant la prison
             Predicate<Rule> predicate = toEraseRule -> toEraseRule.getType() == RuleType.DICE &&
                     rule.getSpecialEffect() == SpecialEffect.DRINK &&
                     ((DiceRule) rule).getDiceValue() == doubleRule.getDoubleValue();
@@ -101,4 +146,24 @@ public class Turn {
     DiceRule makeJail(Integer digit) {
         return new DiceRule(digit, SpecialEffect.DRINK, String.format("Prison de %d Le joueur doit boire une gorgée", digit));
     }
+
+    Rule makeNewRule(RuleType ruleType) {
+
+        switch (ruleType) {
+            case DICE:
+                break;
+            case VALUE:
+                break;
+            case PLAYER:
+                break;
+            case SITUATION:
+                break;
+            case DOUBLE:
+                break;
+            default:
+                break;
+        }
+        return null;
+    }
+
 }
